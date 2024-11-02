98a4c470f9e342e6e04f8c04763ac54369298af3
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2024-11-02 21:04:37 +0100

    disign update

--- index.php
+++ index.php
@@ -8,11 +8,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        body{
+            display : flex;
+            flex-direction :column;
+            align-items:center;
+            justify-content: center ;
+
+        }
+        section{
+            display : flex;
+            flex-direction :column;
+            align-items:center;
+            justify-content: center ;
+            border-radius: 25px;
+            border-color:#000;
+        }
         .container{
             display : flex ;
             flex-direction : column;
+            width: 60%;
             padding: 20px;
-            align-items : center ; 
+            align-items : center ;
+            
             background-color: #669f5266;
             
         }
@@ -24,7 +41,7 @@
   <h1> Résultat de la requête :</h1><br>
  <?php
  // Requête SQL
- $sql = "SELECT * FROM identifiant , employés WHERE id= 2";
+ $sql = "SELECT * FROM personel , poste WHERE personel.id_poste = poste.id_poste";
  $requete = $pdo->prepare($sql);
  
  // Exécuter la requête
@@ -32,23 +49,23 @@
  
  // Récupérer les résultats sous forme d'un tableau associatif
  $utilisateurs = $requete->fetchAll(PDO::FETCH_ASSOC);
- 
+ $nombreUtilisateurs = count($utilisateurs);
  // Vérifier si un résultat est retourné
 
  if (!empty($utilisateurs)) {
     
     
-    foreach ($utilisateurs as $index => $utilisateur) {
+     for ($i = 0; $i < $nombreUtilisateurs; $i++) {
         echo "<div class='container' >";
-        echo "<h2>Utilisateur " . ($index + 1) . " :</h2>";
-        foreach ($utilisateur as $cle => $valeur) {
+        foreach ($utilisateurs[$i] as $cle => $valeur) {
             echo "<strong>$cle :</strong> $valeur <br>";
-        }
-        echo "</div>";
+            }
         echo "<br>"; // Ajoute un saut de ligne entre chaque utilisateur
-
+        echo "</div>";
+            
+        }
     }
- } else {
+  else {
      echo "<h1>Aucun résultat trouvé</h1>";
  }
  ?>
