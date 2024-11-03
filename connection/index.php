188d617f3c40d91cfa7bf81bfdc9dd0314ad880d
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2024-11-03 20:38:40 +0100

    script de connection

--- connection/index.php
+++ connection/index.php
@@ -1,9 +1,13 @@
+<?php
+include "connection.php";
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>connection</title>
+    <title>interraction avec la base de données</title>
 </head>
 <body>
     
