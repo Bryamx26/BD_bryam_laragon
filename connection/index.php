91160d2643d2896499cc5339754c7bdaf9f98ead
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2024-11-03 21:46:57 +0100

    liaison a la base de données

--- connection/index.php
+++ connection/index.php
@@ -1,7 +1,50 @@
 <?php
 include "connection.php";
 ?>
+<?php
+if (isset($_POST["validation"])){
+    
+$nom = htmlspecialchars($_POST["identifiant"]);
+trim($nom);
 
+$motif = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/';
+    
+if (preg_match($motif, $_POST["mot_de_passe"]) and($_POST["mot_de_passe"] != '') ) {
+
+    $mot_de_passe = $_POST["mot_de_passe"];
+} else{
+    $mot_passe = FALSE;
+}
+
+
+if (isset($_POST["identifiant"])) {
+    // Récupération de l'identifiant envoyé par le formulaire
+    $nom = $_POST["identifiant"];
+
+    // Requête SQL pour rechercher un employé par son nom
+    $requete = "SELECT * FROM personel WHERE nom = :nom";
+
+    // Préparation de la requête
+    $prep = $pdo->prepare($requete);
+
+    // Exécution de la requête en liant le paramètre pour éviter les injections SQL
+    $prep->execute([':nom' => $nom]);
+
+    // Récupération des résultats
+    $resultats = $prep->fetchAll(PDO::FETCH_ASSOC);
+
+    // Vérification s'il y a des résultats
+   
+}
+}
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -10,6 +53,44 @@ include "connection.php";
     <title>interraction avec la base de données</title>
 </head>
 <body>
+    <form id="formulaire" action="index.php" method = "post">
+
+    Identifiant : <input type="text" name = "identifiant"> <br>
+
+    mot de passe : <input type="password" name ="mot_de_passe">
+
+    <input type="submit" value="envoyer" name ="validation">
+    </form>
+
+<?php
+if (isset($_POST["validation"])){
+
     
+if (isset($_POST["identifiant"])){
+    echo "Id =" . $nom ."<br>";
+
+}
+if (isset($_POST["mot_de_passe"]) and ($mot_passe == TRUE)){
+    echo "mot de passe :  = ".$_POST["mot_de_passe"];
+
+}else{
+echo"pas de mot de passe.";
+}
+
+if (isset($resultats)) {
+    echo"<section>";
+    // Affichage des résultats
+    foreach ($resultats as $ligne) {
+        echo "Nom : " . $ligne['nom'] . " - Prénom : " . $ligne['prenom'] . "<br>";
+    }
+    echo"</section>";
+} else {
+    echo "Aucun résultat trouvé pour cet identifiant.";
+}
+
+
+}
+?>
+
 </body>
 </html>
